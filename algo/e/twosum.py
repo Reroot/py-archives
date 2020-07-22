c927f35e58a14e975a8835e80b40f1c15fae4b33
artem-reroot <triple8six@gmail.com>
2020-07-21 22:34:24 -0400

    py-1
    Signed-off-by: artem-reroot <triple8six@gmail.com>

--- algo/e/twosum.py
+++ algo/e/twosum.py
@@ -1,9 +1,11 @@
 def twosum(arr, target):
     map = {}
-    for range(x to arr):
-        comp = target-value
-        if(map is comp):
-            return [map[comp], value]
+    for i in range(len(arr)):
+        comp = target-arr[i]#look for thhis value type in our map later
+        if(comp in map):
+            return [map[comp], i]
         else:
-            map = comp
+            map[arr[i]] = i
+            #key is arr[i], and value is the index
 #map contains the comps, and the index as keys
+
