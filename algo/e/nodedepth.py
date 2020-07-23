0380f6497ff018f881696edbd3e2f55d527361b4
artem-reroot <triple8six@gmail.com>
2020-07-23 13:33:55 -0400

    py-1
    Signed-off-by: artem-reroot <triple8six@gmail.com>

--- algo/e/nodedepth.py
+++ algo/e/nodedepth.py
@@ -1,15 +1,32 @@
 #calculate the sum of the depth of each node in the tree
 #root is 0, 2nd level will each have a deppth of 1, sum up all these
 
+#my version rec - needs a fix
 def sumOfEachNodeDepth(node):
     #level order, count, left, and right
     #dfs rec, count +1 on every rec call, each node will get called with
     #the last count it had, update the sum count
     if node is None:
         return 0
-    currDepth = sumOfEachNodeDepth(node.left) + 1
-    currDepth = currDepth + sumOfEachNodeDepth(node.right) + 1
-    return currDepth
+    if node.left is Node and node.right is Node:
+        return 1
+    currdepth = sumOfEachNodeDepth(node.left) + 1
+    currdepth = currdepth + sumOfEachNodeDepth(node.right) + 1
+    return currdepth
+
+
+def sumOfEachNodeDepthFixed( node, nodeSumDepths = 0 ):
+    # level order, count, left, and right
+    # dfs rec, count +1 on every rec call, each node will get called with
+    # the last count it had, update the sum count
+    if node is None:
+        return 0
+    return nodeSumDepths + sumOfEachNodeDepthFixed(node.right, nodeSumDepths + 1) + \
+           sumOfEachNodeDepthFixed(node.left, nodeSumDepths + 1)
+
+
+
+#version 2 - while, with map inside a stack containing the node and depth info
 
 class Node:
     # Constructor to create a new Node
@@ -32,8 +49,8 @@ if __name__ == '__main__':
     root.right.left = Node(6)#2
     root.right.right = Node(7)#2
 
-    root.right.right.left = Node(9)#3
-    root.right.right.right = Node(19)#3
-
+    # root.right.right.left = Node(9)#3
+    # root.right.right.right = Node(19)#3
+    #off by 2 intailly
     print(sumOfEachNodeDepth(root))
-
+    print(sumOfEachNodeDepthFixed(root))
